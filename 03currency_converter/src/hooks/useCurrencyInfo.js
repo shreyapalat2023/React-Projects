d2c160bd24b51d62ee591d9ff2abffde4fd2aa8a
shreyapalat2023 <garimapalat@gmail.com>
2025-02-28 13:55:11 +0530

    done with currency converter

--- 03currency_converter/src/hooks/useCurrencyInfo.js
+++ 03currency_converter/src/hooks/useCurrencyInfo.js
@@ -3,10 +3,10 @@ import { useEffect, useState } from "react"
 function useCurrencyInfo(currency) {
     const [data, setData] = useState()
     useEffect(() => {
-        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/inr.json`)
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@2024-03-06/v1/currencies/${currency}.json`)
             .then((res) => res.json())
             .then((res) => setData(res[currency]))
-        // console.log(data);
+        console.log(data);
 
     }, [currency])
     // console.log(data);
